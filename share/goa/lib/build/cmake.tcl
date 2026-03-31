b606107aa70013f7d0463dd47c26263ab9ff7de3
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2026-03-31 10:15:25 +0200

    cmake: generate missing Find*.cmake files
    Since the Find*.cmake files needed for Goa merely contain boilerplate
    code in most cases, we can scan the CMakeLists.txt for find_package
    directives and generate the .cmake files accordingly. If the lower-case
    package name matches with a used api, we set <PackageName>_FOUND to
    True. Otherwise, we set it to False. The latter prevents the use of
    modules from the host.
    
    We will still keep the support for .cmake files in api archives to
    accomodate for api archives that provide multiple or differently named
    modules.
    
    Fixes genodelabs/goa#169

--- share/goa/lib/build/cmake.tcl
+++ share/goa/lib/build/cmake.tcl
@@ -1,3 +1,71 @@
+proc _create_cmake_modules { } {
+	global config::var_dir config::abi_dir api_dirs project_dir
+	global cmake_module_dir 
+
+	set cmake_module_dir [file join $var_dir cmake Modules]
+	if {[file exists $cmake_module_dir]} {
+		file delete -force $cmake_module_dir }
+
+	file mkdir $cmake_module_dir
+
+	# copy Find*.cmake files from api directories
+	foreach api_dir $api_dirs {
+		foreach cmake_file [glob -nocomplain $api_dir/Find*.cmake] {
+			file copy $cmake_file $cmake_module_dir }
+	}
+
+	# acquire used modules from CMakeLists.txt
+	try {
+		set used_cmake_modules [exec grep find_package\( [file join $project_dir src CMakeLists.txt] | sed -e {s/\s*find_package(\(\w*\).*).*/\1/}]
+	} trap CHILDSTATUS { } {
+		set used_cmake_modules ""
+	} on error { msg } { error $msg $::errorInfo }
+
+	#
+	# create missing Find*.cmake files if corresponding api is used
+	#
+	# Note: We compare the lower-case package name used by cmake with the
+	#       archive names mentioned in used_apis. More precisely, the archive
+	#       name must end with the lower-case package name. If api archives
+	#       provide differently named or multiple packages, they must come with
+	#       corresponding Find*.cmake files.
+	# 
+	foreach package_name $used_cmake_modules {
+		set cmake_file [file join $cmake_module_dir Find${package_name}.cmake]
+		if {[file exists $cmake_file]} {
+			continue }
+
+		# quirk: pthread is provided by our libc
+		if {$package_name == "Threads" && [using_api libc]} {
+			set fd [open $cmake_file w]
+			puts $fd "set(${package_name}_FOUND True)"
+			puts $fd "set(CMAKE_USE_PTHREADS_INIT True)"
+			close $fd
+			continue
+		}
+
+		set api_dir [lsearch -inline -nocase -regexp $api_dirs /.*$package_name/*]
+		if {$api_dir != ""} {
+			set archive_name [lindex [split $api_dir "/"] end-1]
+			diag "CMake module $package_name is provided by used $archive_name api"
+
+			set fd [open $cmake_file w]
+			puts $fd "set(${package_name}_FOUND True)"
+			puts $fd "set(${package_name}_LIBRARY [file join $abi_dir $archive_name.lib.so])"
+			puts $fd "set(${package_name}_INCLUDE_DIR [file join $api_dir include])"
+			close $fd
+		} else {
+			log "CMake module $package_name is not provided by any api archive" \
+			    "please consider adding the corresponding api archive to the used_apis file."
+
+			set fd [open $cmake_file w]
+			puts $fd "set(${package_name}_FOUND False)"
+			close $fd
+		}
+	}
+	return $cmake_module_dir
+}
+
 
 proc create_or_update_build_dir { } {
 
@@ -6,7 +74,6 @@ proc create_or_update_build_dir { } {
 	global env cmake_quirk_args
 	global config::build_dir project_dir config::abi_dir
 	global config::cross_dev_prefix project_name
-	global api_dirs
 
 	# check whether CMakeLists.txt modifies CMAKE_MODULE_PATH
 	set     check_modules_cmd grep
@@ -16,6 +83,8 @@ proc create_or_update_build_dir { } {
 		exit_with_error "src/CMakeLists.txt sets CMAKE_MODULE_PATH, which must be" \
 		                "\nsolely managed by Goa. Please patch the file accordingly."
 	}
+
+	set cmake_module_dir [_create_cmake_modules]
 
 	if {![file exists $build_dir]} {
 		file mkdir $build_dir }
@@ -28,7 +97,7 @@ proc create_or_update_build_dir { } {
 	set     cmd [goa::sandboxed_build_command]
 	lappend cmd cmake
 	lappend cmd "-DCMAKE_IGNORE_PREFIX_PATH='/;/usr'"
-	lappend cmd "-DCMAKE_MODULE_PATH='[join ${api_dirs} ";"];[file join $tool_dir cmake Modules]'"
+	lappend cmd "-DCMAKE_MODULE_PATH='$cmake_module_dir;[file join $tool_dir cmake Modules]'"
 	lappend cmd "-DCMAKE_SYSTEM_NAME=Genode"
 	lappend cmd "-DCMAKE_C_COMPILER=${cross_dev_prefix}gcc"
 	lappend cmd "-DCMAKE_CXX_COMPILER=${cross_dev_prefix}g++"
